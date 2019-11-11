ed7a10bf9a32f64898aded3e963db0c585e6b489
Rose Seraphin <roseseraphin@yahoo.com>
2019-11-11 02:51:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,8 +13,6 @@ end
 
 def age_appropriate_birthday(birthday_kids)
  
-  
-  
   birthday_kids.collect do |kids_name, age|
    if age <= 12 
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
